d512e3669e6e0310007bc59f2acafec25e3044a9
Md Ashickur Rahman <ashickur.noor@gmail.com>
2024-01-20 16:05:36 +0600

    3.8 with notes

--- src/components/TravelPlan.jsx
+++ src/components/TravelPlan.jsx
@@ -1,42 +1,41 @@
-import { initialTravelPlan } from '../data/places-normalized';
+import { initialTravelPlan } from "../data/places-normalized";
 
-import {useState} from 'react';
-import PlaceTree from './PlaceTree';
+import { useState } from "react";
+import PlaceTree from "./PlaceTree";
 
 export default function TravelPlan() {
-  const [plan, setPlan]=useState(initialTravelPlan);
-  const root=plan[0];
-  const planetIds=root.childIds;
-    console.log(plan);
- const handleVisited=(parentId,visitedPlaceId)=>{
-    const parent=plan[parentId];
-    const updateParent={
-        ...parent,
-        childIds:parent.childIds.filter(id=>id!==visitedPlaceId)
-    }
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const root = plan[0];
+  const planetIds = root.childIds;
+  console.log(plan);
+  const handleVisited = (parentId, visitedPlaceId) => { // parentId is the id of the parent of the visited place, visitedPlaceId is the id of the visited place
+    const parent = plan[parentId]; // get the parent object
+    const updateParent = { // create a new parent object
+      ...parent, //
+      childIds: parent.childIds.filter((id) => id !== visitedPlaceId), // remove visited place from childIds
+    };
+    
     setPlan({
-        ...plan,
-        [parentId]:updateParent
-    })
- }
-  return(
+      ...plan, // copy the plan object
+      [parentId]: updateParent, // update the parent object
+    });
+  };
+  return (
     <>
       <h2>Places To Visit</h2>
-        <ol>
-            {
-                planetIds.map((placeId)=>{
-                    return(
-                        <PlaceTree 
-                        key={placeId} 
-                        currentPlaceId={placeId} 
-                        allPlaces={plan} 
-                        onVisitClick={handleVisited}
-                        parentId={root.id}
-                        />
-                    )
-                })
-            }
-        </ol>
+      <ol>
+        {planetIds.map((placeId) => {
+          return (
+            <PlaceTree
+              key={placeId}
+              currentPlaceId={placeId}
+              allPlaces={plan}
+              onVisitClick={handleVisited}
+              parentId={root.id}
+            />
+          );
+        })}
+      </ol>
     </>
-  )
+  );
 }
