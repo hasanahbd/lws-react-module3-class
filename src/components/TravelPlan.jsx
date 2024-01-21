1615da1c46877bab5d41dcccd07927712770d373
Md Ashickur Rahman <ashickur.noor@gmail.com>
2024-01-21 12:21:10 +0600

    3.8 with immer1

--- src/components/TravelPlan.jsx
+++ src/components/TravelPlan.jsx
@@ -3,12 +3,12 @@ import { useImmer } from "use-immer";
 import PlaceTree from "./PlaceTree";
 
 export default function TravelPlan() {
-
   const [plan, updatePlan] = useImmer(initialTravelPlan);
   console.log(plan);
   const root = plan[0];
   const planetIds = root.childIds;
-  function deletePlaces(draft, placeId) { // draft is the plan, placeId is the id of the place to be deleted
+  function deletePlaces(draft, placeId) {
+    // draft is the plan, placeId is the id of the place to be deleted
     const place = draft[placeId];
     place.childIds.forEach((childId) => {
       deletePlaces(draft, childId);
@@ -17,7 +17,8 @@ export default function TravelPlan() {
   }
   const handleVisited = (parentId, visitedPlaceId) => {
     // parentId is the id of the parent of the visited place, visitedPlaceId is the id of the visited place
-    updatePlan((draft) => { // draft is the plan, it is nature of useImmer
+    updatePlan((draft) => {
+      // draft is the plan, it is nature of useImmer
       const parentPlace = draft[parentId]; // parentPlace is the parent of the visited place
       parentPlace.childIds = parentPlace.childIds.filter(
         (id) => id !== visitedPlaceId
